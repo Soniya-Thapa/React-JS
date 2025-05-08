a3b9affb60b5d5cfe610b081f958906ac8949c70
Soniya-Thapa <ayinos.apaht143@gmail.com>
2025-05-08 20:35:19 +0545

    routing in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-//yo tala ko function ko jasto lagla tara function haina yo "component" ho cause yesley jsx return gariraxa ... function bhaneko yo ho : function App(){ console.log("hello")} cause yesley jsx return gareko xaina
+//yo tala ko function ko jasto lagla tara function haina, yo "component" ho cause yesley jsx return gariraxa ... function bhaneko yo ho : function App(){ console.log("hello")} cause yesley jsx return gareko xaina
+
+import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 function App() {
   //js ko code component ra return ko bich ma lekhna parxa 
-  let name = "Soniya"
-  let address = "Kaushaltar"
+  // let name = "Soniya"
+  // let address = "Kaushaltar"
 
   return (
     //browser ma dekhaunu parne xa bhani return bhitra lekhna parxa
     //<>:react fragments , dom ma entry hudaina ra js ko code lekhna xa bhani sadhai {} bhitra lekhna
-    <>
-      <h1>Hello World {1 + 1}</h1>
-      <h2>Hello {name}!!! Your address is : {address}</h2> 
-      <h3>Bye World</h3>
-    </>
+    // <>
+    //   <h1 style={
+    //     {color : "yellow",backgroundColor : "blue"}
+    //     }>
+    //       Hello World {1 + 1}</h1>
+    //   <h2>Hello {name}!!! Your address is : {address}</h2> 
+    //   <h3>Bye World</h3>
+    // </>
+
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<h1>This is home page</h1>}/>
+        <Route path="/about" element = {<h1>This is about page  </h1>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
 
 export default App
+
+
