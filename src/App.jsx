faebb29750fd8d11e4ed76aa1d63ebd558102284
Soniya-Thapa <ayinos.apaht143@gmail.com>
2025-05-12 20:34:13 +0545

    tailwind css note

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
 //yo tala ko function ko jasto lagla tara function haina, yo "component" ho cause yesley jsx return gariraxa ... function bhaneko yo ho : function App(){ console.log("hello")} cause yesley jsx return gareko xaina
 
 import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Home from "./Home"
-import About from "./About"
-import Contact from "./Contact"
-import LearningUseEffect from "./LearningUseEffect"
+import HomePage from "./pages/HomePage"
+import SinglePage from "./pages/SinglePage"
+import CreatePage from "./pages/CreatePage"
+import EditPage from "./pages/EditPage"
 
 function App() {
-  // js ko code component ra return ko bich ma lekhna parxa 
-  // let name = "Soniya"
-  // let address = "Kaushaltar"
 
   return (
-    //browser ma dekhaunu parne xa bhani return bhitra lekhna parxa
-    //<>:react fragments , dom ma entry hudaina ra js ko code lekhna xa bhani sadhai {} bhitra lekhna
-    // <>
-    //   <h1 style={
-    //     {color : "yellow",backgroundColor : "blue"}
-    //     }>
-    //       Hello World {1 + 1}</h1>
-    //   <h2>Hello {name}!!! Your address is : {address}</h2> 
-    //   <h3>Bye World</h3>
-    // </>
     <BrowserRouter>
       <Routes>
-{/* //jasari function lai hello() garera invoke garinxa tyesari nai componentlai <Home /> yesari invoke/call garinxa */}
-        <Route path = "/" element = {<Home />} />
-        <Route path = "/about" element = {<About />} />
-        <Route path = "/contact" element = {<Contact />} />
-        <Route path = "/learninguseeffect" element = {<LearningUseEffect />} />
-        <Route path="/" element = {<h1>This is home page</h1>}/>
-        <Route path="/about" element = {<h1>This is about page  </h1>}/>
+        <Route path = "/" element = {<HomePage />} />
+        <Route path = "/single-page" element = {<SinglePage />} />
+        <Route path = "/create-page" element = {<CreatePage />} />
+        <Route path = "/edit-page" element = {<EditPage />} />
       </Routes>
     </BrowserRouter>
   )
