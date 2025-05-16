146bd30d0bccfa4354ce52778901d95a45295b10
Soniya-Thapa <ayinos.apaht143@gmail.com>
2025-05-16 15:44:26 +0545

    card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,29 @@
 import Button from "../components/Button"
+import Card from "../components/Card"
 import Navbar from "../components/Navbar"
 
 function HomePage() {
   return (
     <>
       <Navbar />
-      <h1>This is Home Page !!!</h1>
-      <h1 className="text-blue-300 bg-amber-950">Tailwind is successfully exported !!!</h1>
-      
+
+      <div className="flex-wrap flex">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      {/* <h1>This is Home Page !!!</h1>
+      <h1 className="text-blue-300 bg-amber-950">Tailwind is successfully exported !!!</h1> */}
+
       {/* <Button title="Login" />
       <Button title="Create" />
       <Button title="Register" /> */}
